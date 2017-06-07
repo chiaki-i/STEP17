3de0c1163824ba09d33a789af04e84c0573db6f5
Chiaki ISHIO <g1420505@is.ocha.ac.jp>
2017-06-07 12:50:43 +0900

    try data n = 0 to 360

--- week2/my_matrix.py
+++ week2/my_matrix.py
@@ -1,44 +1,59 @@
-import numpy, sys, time, pandas
+import numpy, sys, time
+import csv, pandas 
 
-if (len(sys.argv) != 2):
-    print ("usage: python %s N" % sys.argv[0])
-    quit()
+def calc(n):
+    '''
+    if (len(sys.argv) != 2):
+        print ("usage: python %s N" % sys.argv[0])
+        quit()
+        '''
 
-n = int(sys.argv[1])
-a = numpy.zeros((n, n)) # Matrix A
-b = numpy.zeros((n, n)) # Matrix B
-c = numpy.zeros((n, n)) # Matrix C
+    # n = int(sys.argv[1])
+    a = numpy.zeros((n, n)) # Matrix A
+    b = numpy.zeros((n, n)) # Matrix B
+    c = numpy.zeros((n, n)) # Matrix C
 
-# Initialize the matrices to some values.
-for i in range(n): # xrange を range に
-    for j in range(n):
-        a[i, j] = i * n + j
-        b[i, j] = j * n + i
-        c[i, j] = 0
+    # Initialize the matrices to some values.
+    for i in range(n): # xrange を range に
+        for j in range(n):
+            a[i, j] = i * n + j
+            b[i, j] = j * n + i
+            c[i, j] = 0
 
-begin = time.time()
+    begin = time.time()
 
-######################################
-# Write code to calculate C = A * B. #
-######################################
+    ######################################
+    # Write code to calculate C = A * B. #
+    ######################################
 
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            c[i, j] += a[i, k] * b[k, j]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                c[i, j] += a[i, k] * b[k, j]
 
-######################################
+    ######################################
 
-end = time.time()
-print ("time: %.6f sec" % (end - begin))
+    end = time.time()
+    result = format((end - begin), '5.10f')
+    return (str(n).center(10, ' ') + str(result) + '\n')
+    # print ("time: %.6f sec" % (end - begin))
 
-# Print C for debugging. Comment out the print before measuring the execution time.
-total = 0
-for i in range(n):
-    for j in range(n):
-        # print(c[i, j])
-        total += c[i, j]
-# Print out the sum of all values in C.
-# This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
-print ("sum: %.6f" % total)
+    """
+    # Print C for debugging. Comment out the print before measuring the execution time.
+    total = 0
+    for i in range(n):
+        for j in range(n):
+            # print(c[i, j])
+            total += c[i, j]
+    # Print out the sum of all values in C.
+    # This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
+    print ("sum: %.6f" % total)
+    """
 
+if __name__ == '__main__':
+    file = open('matrix.dat', 'w')
+    for i in range(100):
+        # for j in range(5):
+            file.write(calc(i*10))
+    file.close()
+    
